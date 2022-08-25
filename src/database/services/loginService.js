74ce0f28b6757ac669037c0d162eee1bc3edc66b
Amanda Soares <amandchen@hotmail.com>
2022-08-25 17:51:42 -0300

    [feat]: Cria rota POST /login

---
+++ src/database/services/loginService.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const CustomError = require('../middlewares/CustomError');
+const { User } = require('../models');
+
+const generateToken = async (userData) => {
+  const jwtConfig = {
+    expiresIn: '4d',
+    algorithm: 'HS256',
+  };
+
+  const jwtSecret = process.env.JWT_SECRET;
+  const token = jwt.sign({ data: userData }, jwtSecret, jwtConfig);
+  return token;
+};
+
+const getUser = async ({ email, password }) => {
+  if (!email || !password) {
+    throw new CustomError(400, 'Some required fields are missing');
+  }
+
+  const checkUser = await User.findOne({ where: { email } });
+
+  if (!checkUser || checkUser.password !== password) {
+    throw new CustomError(400, 'Invalid fields');
+  }
+
+  const token = generateToken(checkUser);
+  return token;
+};
+
+module.exports = {
+  getUser,
+};
--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
+require('express-async-errors');
 const app = require('./api');
+const { login } = require('./database/controllers/loginController');
+const errorMiddleware = require('./database/middlewares/errorMiddleware');
 
 // não remova a variável `API_PORT` ou o `listen`
 const port = process.env.API_PORT || 3000;
@@ -8,5 +11,8 @@ const port = process.env.API_PORT || 3000;
 app.get('/', (_request, response) => {
   response.send();
 });
+app.post('/login', login);
+
+app.use(errorMiddleware);
 
 app.listen(port, () => console.log('ouvindo porta', port));
