e8d0fa337aa9eea4457bb5bd2d6675dc18f2e0cb
Amanda Soares <amandchen@hotmail.com>
2022-08-26 17:54:44 -0300

    [fix] Corrige alias de User em model de BlogPost

--- src/database/models/blogPost.js
+++ src/database/models/blogPost.js
@@ -1,5 +1,6 @@
 module.exports = (sequelize, DataTypes) => {
   const BlogPost = sequelize.define('BlogPost', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, allowNull: false },
     title: DataTypes.STRING,
     content: { type: DataTypes.STRING, unique: true },
     userId: DataTypes.INTEGER,
